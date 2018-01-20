84f827d448e8c4986b96c199b49b2215e8e225e2
vdelvalle <victor.delvalle@osoco.es>
2018-01-20 16:58:37 +0100

    wip

--- app.js
+++ app.js
@@ -9,22 +9,36 @@ app.use(methodOverride());
 
 var router = express.Router();
 
+var mnemonicAbi = require("./contracts/MnemonicVault.json");
 router.get('/checkMe', function(req, res) {
    	var Web3 = require('web3');
-	web3 = new Web3(new Web3.providers.HttpProvider('https://ropsten.infura.io/'));
-	console.log('Getting Ethereum address info.....');
-	var addr = ('0x627306090abaB3A6e1400e9345bC60c78a8BEf57');
-	console.log('Address:', addr);
-	web3.eth.getBalance(addr, function (error, result) {
-		if (!error) {
-			res.send('Ether:' + result);
-			console.log('Ether: '+ result); // Show the ether balance after converting it from Wei
-		}else{
-			console.log('Huston we have a promblem: ', error); // Should dump errors here
-			res.send('Ether: ' +  error);
-		}
-	});
+	web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:7545/'));
+	var myAddress = ('0x627306090abaB3A6e1400e9345bC60c78a8BEf57');
+	var myHash = '66d02f609dac679a7f0b1424f3412e6a3a14c67eeef2cd102ff2e1e2fa9ed432';
+ 	var requestedKey = 'Room42'
+
+
+    //var mNemonicVaultContract = web3.eth.contract(mnemonicAbi.abi).at(req.query.Address);
+    console.log("contract retrieving... ");
+    console.log("abi: " + mnemonicAbi.abi);
+    var mNemonicVaultContract = web3.eth.contract(mnemonicAbi.abi).at("0xa0755256eef2f06c9add08a43b64b5ac96051f153a7ddeb1b1348586aafe10e5");
+    
+    //var result = mNemonicVaultContract.retrieveDocument("0x8cdaf0cd259887258bc13a92c0a6da92698644c0", requestedKey);
+
+
+
+/*
+	MnemonicVaultContract.methods.retrieveDocument()
+	.call()
+	.then(console.log);
+*/
 });
+
+/*
+function checkContractHash(hash){
+	hash 
+}
+*/
 
 app.use(router);
 
---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  entry: './app.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'app.js'
+  },
+  plugins: [
+    // Copy our app's index.html to the build folder.
+    new CopyWebpackPlugin([
+    ])
+  ],
+  module: {
+    rules: [
+      {
+       test: /\.css$/,
+       use: [ 'style-loader', 'css-loader' ]
+      }
+    ],
+    loaders: [
+      { test: /\.json$/, use: 'json-loader' },
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015'],
+          plugins: ['transform-runtime']
+        }
+      }
+    ]
+  }
+}
