19864f9e09ddb13ce1323e8fa8d671cb024ffee6
vdelvalle <victor.delvalle@osoco.es>
2018-01-20 13:54:12 +0100

    wip

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ var express = require("express"),
     app = express(),
     bodyParser  = require("body-parser"),
     methodOverride = require("method-override");
-    mongoose = require('mongoose');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -11,7 +10,20 @@ app.use(methodOverride());
 var router = express.Router();
 
 router.get('/', function(req, res) {
-   res.send("Hello World!");
+   var Web3 = require('web3');
+	web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:7545'));
+	console.log('Getting Ethereum address info.....');
+	var addr = ('0x627306090abaB3A6e1400e9345bC60c78a8BEf57');
+	console.log('Address:', addr);
+	web3.eth.getBalance(addr, function (error, result) {
+		if (!error) {
+			res.send('Ether:' + web3.utils.fromWei(result,'ether'));
+			console.log('Ether: '+ web3.utils.fromWei(result,'ether')); // Show the ether balance after converting it from Wei
+		}else{
+			console.log('Huston we have a promblem: ', error); // Should dump errors here
+			res.send('Ether: ' +  error);
+		}
+	});
 });
 
 app.use(router);
